7c2c5b973db375191a4b6caa276aeb694d042094
JonathanMary <jon85ma@gmail.com>
2021-01-19 22:38:57 +0100

    Comments: Done, Posts: Ongoing, SearchBar: None

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from './components/Posts/Posts'
 // Import the dummyData
+import dummyData from './dummy-data'
+
 import './App.css';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+    const [posts, setPosts] = useState(dummyData);
+    //const searchbar, in stretch.
+    console.log("'App.js file', posts: \n", posts)
+    console.log("'App.js file', Post Array: \n", Posts)
 
   const likePost = postId => {
     /*
@@ -32,6 +39,8 @@ const App = () => {
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      {<Posts posts={posts} />}
+      {/*<Posts />*/}
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -5,10 +5,15 @@ import './Comments.css';
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
+  console.log("Comments.js, comments: ", comments);
 
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map(comment => {
+        //console.log("Comment.js, comment: ", comment)
+        return <Comment key={comment.id} comment={comment} />
+      })}
     </div>
   );
 };
